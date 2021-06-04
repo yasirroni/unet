aebad9cefab31545214e5d91dfca74ac0ab1fe73
yasirroni <muhammadyasirroni@gmail.com>
2021-06-04 13:39:27 +0700

    initial replication base

--- main.py
+++ main.py
@@ -18,53 +18,54 @@ from utils import create_gif
 
 
 def run(flags_obj):
-  data_aug_args = dict(rotation_range=0.2,
-                       width_shift_range=0.05,
-                       height_shift_range=0.05,
-                       shear_range=0.05,
-                       zoom_range=0.05,
-                       horizontal_flip=True,
-                       fill_mode='nearest')
+    data_aug_args = dict(rotation_range=0.2,
+                         width_shift_range=0.05,
+                         height_shift_range=0.05,
+                         shear_range=0.05,
+                         zoom_range=0.05,
+                         horizontal_flip=True,
+                         fill_mode='nearest'
+                        )
 
-  train_gene = train_generator(flags_obj, data_aug_args)
+    train_gene = train_generator(flags_obj, data_aug_args)
 
-  model = unet(flags_obj, n_filters=64)
+    model = unet(flags_obj, n_filters=64)
 
-  model.compile(
-      optimizer=tf.keras.optimizers.Adam(learning_rate=flags_obj.learning_rate),
-      loss=tf.keras.losses.BinaryCrossentropy(),
-      metrics=['accuracy'])
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=flags_obj.learning_rate),
+        loss=tf.keras.losses.BinaryCrossentropy(),
+        metrics=['accuracy'])
 
-  example = load_example(flags_obj)
-  example_img = imageio.imread('data/membrane/test/image/0.png')
-  # Save first prediction without training.
-  save_prediction(model, example_img, example, 0)
+    example = load_example(flags_obj)
+    example_img = imageio.imread('data/membrane/test/image/0.png')
+    # Save first prediction without training.
+    save_prediction(model, example_img, example, 0)
 
-  test_ds = load_test_dataset()
+    test_ds = load_test_dataset()
 
-  history = model.fit_generator(
-      train_gene,
-      epochs=flags_obj.epoch,
-      steps_per_epoch=flags_obj.steps_per_epoch,
-      validation_data=test_ds,
-      callbacks=[DisplayCallback(model, example)])
+    history = model.fit_generator(
+        train_gene,
+        epochs=flags_obj.epoch,
+        steps_per_epoch=flags_obj.steps_per_epoch,
+        validation_data=test_ds,
+        callbacks=[DisplayCallback(model, example)])
 
-  create_gif()
-  plot_history(history, flags_obj.epoch)
+    create_gif()
+    plot_history(history, flags_obj.epoch)
 
-  ## Show model architecture.
-  # tf.keras.utils.plot_model(
-  #   model,
-  #   to_file='model.png',
-  #   show_shapes=True
-  # )
+    ## Show model architecture.
+    # tf.keras.utils.plot_model(
+    #   model,
+    #   to_file='model.png',
+    #   show_shapes=True
+    # )
 
 def main(_):
-  if not os.path.isdir('samples'):
-    os.mkdir('samples')
-  run(flags.FLAGS)
+    if not os.path.isdir('samples'):
+        os.mkdir('samples')
+    run(flags.FLAGS)
 
 
 if __name__ == '__main__':
-  define_flags()
-  app.run(main)
+    define_flags()
+    app.run(main)
